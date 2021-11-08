ad494d4f2759f6c57bedbaaa81ae30f8b128ce07
John Manzardo <jmanzard@gmail.com>
2021-11-08 10:56:38 -0600

    First Commit

--- edu/cs300/ReportingSystem.java
+++ edu/cs300/ReportingSystem.java
@@ -5,6 +5,104 @@ import java.io.FileNotFoundException;
 import java.util.Enumeration;
 import java.util.Scanner;
 import java.util.Vector;
+import java.io.FileWriter;
+
+class NewThread extends Thread {
+	int reportId = -1;			//Updated when threads are created
+	String specFileName;		//Name of each spec file
+	String reportTitle;
+	String searchString;
+	String outFileName;
+	Vector<Integer> begCol = new Vector<Integer>();
+	Vector<Integer> endCol = new Vector<Integer>();
+	Vector<String> colHeading = new Vector<String>();
+	int numReports = 0;
+
+	public void run() {
+		System.out.println(specFileName);		//DELETE: confirms specID has been updated
+		System.out.println(reportId);		//DELETE: confirms reportID has been updated
+		//int numRecords = 0;	//DELETE
+		try {
+			File specFile = new File(specFileName);		//Opens each report spec file
+
+			Scanner reportSpec = new Scanner(specFile);
+
+			
+			
+			reportTitle = reportSpec.nextLine();
+			searchString = reportSpec.nextLine();
+			outFileName = reportSpec.nextLine();
+			
+			while(reportSpec.hasNext()) {
+				reportSpec.useDelimiter("-");		//Changes delimiter to '-' in order to get beg col value
+
+				begCol.add(reportSpec.nextInt());		//Adds beg col value to vector
+				//System.out.println(begCol.get(numRecords));	//DELETE, just for Testing
+				
+				reportSpec.useDelimiter(",");		//Changes delimiter to '-' in order to get end col value
+
+				endCol.add(reportSpec.skip("-").nextInt());		//Adds end col value to vector
+				//System.out.println(endCol.get(numRecords));	//DELETE, just for Testing
+
+				reportSpec.reset();					//Resets delimiter to default value
+
+				colHeading.add(reportSpec.skip(",").nextLine());		//Gets remainder of line
+				//System.out.println(colHeading.get(numRecords));	//DELETE, just for Testing
+
+				//numRecords++;			//DELETE, just for Testing
+			}
+			
+
+			reportSpec.close();
+
+		} catch (FileNotFoundException ex) {
+			System.out.println("FileNotFoundException triggered:"+ex.getMessage());
+		}
+
+		try {
+
+			MessageJNI.writeReportRequest(reportId, numReports, searchString);
+
+			File outFile = new File(outFileName);
+			outFile.createNewFile();		//Create file for new report
+
+			FileWriter outWriter = new FileWriter(outFileName);
+
+			String recRecord = MessageJNI.readReportRecord(reportId);
+
+				outWriter.write(reportTitle + "\t\n");
+				for (int i = 0; i < colHeading.size(); i++) {
+					outWriter.write(colHeading.get(i));
+					if (i != colHeading.size() - 1) {
+						outWriter.write("\t");
+					}
+					else{
+						outWriter.write("\n");
+					}
+				}
+			while (recRecord.length() > 1) {
+				for (int i = 0; i < colHeading.size(); i++) {
+					String recRecordSub = recRecord.substring(begCol.get(i) - 1, endCol.get(i));
+					outWriter.write(recRecordSub);
+					if (i != colHeading.size() - 1) {
+						outWriter.write("\t");
+					}
+					else{
+						outWriter.write("\n");
+					}
+				}
+				recRecord = MessageJNI.readReportRecord(reportId);
+				//System.out.println(reportId + ": HERE");
+
+			}
+			outWriter.close();
+
+			//System.out.println(MessageJNI.readReportRecord(reportId));
+		} catch (Exception e) {
+			System.err.println("Error" + e);
+		}
+	}
+}
 
 public class ReportingSystem {
 
@@ -16,15 +114,34 @@ public class ReportingSystem {
 	public int loadReportJobs() {
 		int reportCounter = 0;
 		try {
+			
+			File file = new File ("report_list.txt");
 
-			   File file = new File ("report_list.txt");
-
-			   Scanner reportList = new Scanner(file);
+			Scanner reportList = new Scanner(file);
 
  		     //load specs and create threads for each report
-				 DebugLog.log("Load specs and create threads for each report\nStart thread to request, process and print reports");
+			Vector<String> filenames = new Vector<String>();
+			int numReps = 0;
+			String whiteSpace;
+  
+			numReps = reportList.nextInt();
+			System.out.println(numReps);		//DELETE: confirms number of reports in report_list.txt
+			whiteSpace = reportList.nextLine();
+			while (reportList.hasNextLine()) {
+				filenames.add(reportList.nextLine());
+			}
+			System.out.println(filenames.get(0));
 
-			   reportList.close();
+				DebugLog.log("Load specs and create threads for each report\nStart thread to request, process and print reports");
+			for (int i = 0; i < numReps; i++) {		//creates a new thread for each report i < numReps
+				NewThread reportThread = new NewThread();
+				reportThread.reportId = i + 1;		//assigns correct index to ID
+				reportThread.specFileName = filenames.get(i);
+				reportThread.numReports = numReps;
+				reportThread.start();
+			}
+
+			reportList.close();
 		} catch (FileNotFoundException ex) {
 			  System.out.println("FileNotFoundException triggered:"+ex.getMessage());
 		}
--- process_records.c
+++ process_records.c
@@ -7,6 +7,55 @@
 #include <errno.h>
 #include "report_record_formats.h"
 #include "queue_ids.h"
+#include <pthread.h>
+#include <unistd.h>
+#include <signal.h>
+#include <semaphore.h>
+int totalRecords = 0;       //KEEPS TRACK OF NUM RECORDS
+int totalReports = 0;       //KEEPS TRACK OF NUM REPORTS
+
+pthread_cond_t condVar = PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+sem_t s;
+
+
+typedef struct {
+    int recordsSent;
+} counts_t;
+
+//overrides signint and changes condition variable, triggering the thread that prints the status report
+void handle_sigint(int sig) {
+    pthread_cond_signal(&condVar);
+    signal(SIGINT, handle_sigint);
+    
+}
+
+//pthread to print out status report
+void *srThread (void *arg) {
+    int val;
+
+    int *repSentArray = (int *)arg;
+
+    sem_getvalue(&s, &val);
+    
+    pthread_mutex_lock(&mutex);
+    pthread_cond_wait(&condVar, &mutex);
+
+    do {
+        pthread_mutex_lock(&mutex);
+        pthread_cond_wait(&condVar, &mutex);
+
+        printf("\n***Report***\n");
+        printf("%d records read for %d reports\n", totalRecords, totalReports);
+        int i;
+        for (i = 0; i < totalReports; i++) {
+            printf("Records sent for report index %d: %d\n", i + 1, repSentArray[0]);
+        }
+        pthread_mutex_unlock(&mutex);
+        sem_getvalue(&s, &val);
+
+    } while (val == 1);
+}
 
 int main(int argc, char**argv)
 {
@@ -17,6 +66,8 @@ int main(int argc, char**argv)
     report_record_buf sbuf;
     size_t buf_length;
 
+
+    //RECEIVE MESSAGE FROM JAVA PROGRAM
 
     key = ftok(FILE_IN_HOME_DIR,QUEUE_NUMBER);
     if (key == 0xffffffff) {
@@ -47,70 +98,155 @@ int main(int argc, char**argv)
       }
     } while ((ret < 0 ) && (errno == 4));
     //fprintf(stderr,"msgrcv error return code --%d:$d--",ret,errno);
-
+    
     fprintf(stderr,"process-msgrcv-request: msg type-%ld, Record %d of %d: %s ret/bytes rcv'd=%d\n", rbuf.mtype, rbuf.report_idx,rbuf.report_count,rbuf.search_string, ret);
 
-    key = ftok(FILE_IN_HOME_DIR,1);
-    if (key == 0xffffffff) {
-        fprintf(stderr,"Key cannot be 0xffffffff..fix queue_ids.h to link to existing file\n");
-        return 1;
+
+
+    report_request_buf recordsRead[rbuf.report_count];
+    recordsRead[0] = rbuf;
+    totalReports = recordsRead[0].report_count;
+    int k;
+    for (k = 1;  k < rbuf.report_count; k++) {
+        do {
+            ret = msgrcv(msqid, &recordsRead[k], sizeof(rbuf), 1, 0);//receive type 1 message
+
+            int errnum = errno;
+            if (ret < 0 && errno !=EINTR){
+                fprintf(stderr, "Value of errno: %d\n", errno);
+                perror("Error printed by perror");
+                fprintf(stderr, "Error receiving msg: %s\n", strerror( errnum ));
+            }
+            } while ((ret < 0 ) && (errno == 4));
+            //fprintf(stderr,"msgrcv error return code --%d:$d--",ret,errno);
+
+            fprintf(stderr,"process-msgrcv-request: msg type-%ld, Record %d of %d: %s ret/bytes rcv'd=%d\n", rbuf.mtype, rbuf.report_idx,rbuf.report_count,rbuf.search_string, ret);
+    }
+
+    //Hook signaler now that all messages have been read in
+    signal(SIGINT, handle_sigint);
+
+    //Create int array to store how many records have been sent back to ReportingSystem
+    int repSentArray[rbuf.report_count];
+    int j = 0;
+    for (j = 0; j < rbuf.report_count; j++) {
+        repSentArray[j] = 0;
+    }
+
+    //CREATE THREAD THAT WILL BE USED TO SEND STATUS REPORT
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, srThread, (void *) repSentArray);
+
+    //sleep(5);
+
+
+    char field[RECORD_FIELD_LENGTH];        //Field for reading in each record
+
+    while (fgets(field, RECORD_FIELD_LENGTH, stdin) != NULL) {
+        if (strcmp(field, "\n") != 0) {
+            pthread_mutex_lock(&mutex);
+            totalRecords++;
+            pthread_mutex_unlock(&mutex);
+
+        }
+        if (totalRecords % 10 == 0) {
+            sleep(5);
+        }
+
+        int i;
+        for (i = 0; i < rbuf.report_count; i++) {   //checks if current record contains any of the search strings
+
+            //SEND MESSAGE TO JAVA PROGRAM
+
+            key = ftok(FILE_IN_HOME_DIR, recordsRead[i].report_idx);
+            if (key == 0xffffffff) {
+                fprintf(stderr,"Key cannot be 0xffffffff..fix queue_ids.h to link to existing file\n");
+                return 1;
+            }
+
+
+            if ((msqid = msgget(key, msgflg)) < 0) {
+                int errnum = errno;
+                fprintf(stderr, "Value of errno: %d\n", errno);
+                perror("(msgget)");
+                fprintf(stderr, "Error msgget: %s\n", strerror( errnum ));
+            }
+            else
+                fprintf(stderr, "msgget: msgget succeeded: msgqid = %d\n", msqid);
+
+
+                //PARSES records.mini IN ORDER TO SEND CORRECT VALUES
+            if (strstr(field, recordsRead[i].search_string) != NULL) {        //CHEKCS IF SEARCH STRING IS IN EACH RECORD             //if (strstr(field, rbuf.search_string) != NULL)
+                //SEND TO CORRESPONDING MESSAGE QUEQUE
+                sbuf.mtype = 2;
+                    strcpy(sbuf.record,field); 
+                    buf_length = strlen(sbuf.record) + sizeof(int)+1;//struct size without
+                    // Send a message.
+                    if((msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT)) < 0) {
+                        int errnum = errno;
+                        fprintf(stderr,"%d, %ld, %s, %d\n", msqid, sbuf.mtype, sbuf.record, (int)buf_length);
+                        perror("(msgsnd)");
+                        fprintf(stderr, "Error sending msg: %s\n", strerror( errnum ));
+                        exit(1);
+                    }
+                    else
+                        fprintf(stderr,"msgsnd-report_record: record\"%s\" Sent (%d bytes)\n", sbuf.record,(int)buf_length);
+                        repSentArray[i]++;
+            } 
+
+        }
+
+    }
+  
+
+    //Now that all matching records have been sent, send zero length message to each running thread
+    for (k = 0; k < recordsRead[0].report_count; k++) {
+
+            key = ftok(FILE_IN_HOME_DIR, recordsRead[k].report_idx);
+            if (key == 0xffffffff) {
+                fprintf(stderr,"Key cannot be 0xffffffff..fix queue_ids.h to link to existing file\n");
+                return 1;
+            }
+
+
+            if ((msqid = msgget(key, msgflg)) < 0) {
+                int errnum = errno;
+                fprintf(stderr, "Value of errno: %d\n", errno);
+                perror("(msgget)");
+                fprintf(stderr, "Error msgget: %s\n", strerror( errnum ));
+            }
+            else
+                fprintf(stderr, "msgget: msgget succeeded: msgqid = %d\n", msqid);
+
+
+        // Sends the zero legnth message to let ReportingSystem know no more records will be sent
+        sbuf.mtype = 2;
+        strcpy(sbuf.record, "");
+        buf_length = strlen(sbuf.record) + sizeof(int)+1;//struct size without
+        // Send a message.
+        if((msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT)) < 0) {
+            int errnum = errno;
+            fprintf(stderr,"%d, %ld, %s, %d\n", msqid, sbuf.mtype, sbuf.record, (int)buf_length);
+            perror("(msgsnd)");
+            fprintf(stderr, "Error sending msg: %s\n", strerror( errnum ));
+            exit(1);
+        }
+        else
+            fprintf(stderr,"msgsnd-report_record: record\"%s\" Sent (%d bytes)\n", sbuf.record,(int)buf_length);
+
     }
 
 
-    if ((msqid = msgget(key, msgflg)) < 0) {
-        int errnum = errno;
-        fprintf(stderr, "Value of errno: %d\n", errno);
-        perror("(msgget)");
-        fprintf(stderr, "Error msgget: %s\n", strerror( errnum ));
+    //Print the final records report and then exit program
+    printf("***Report***\n");
+    printf("%d records read for %d reports\n", totalRecords, totalReports);
+    for (k = 0; k < totalReports; k++) {
+        printf("Records sent for report index %d: %d\n", k + 1, repSentArray[k]);
     }
-    else
-        fprintf(stderr, "msgget: msgget succeeded: msgqid = %d\n", msqid);
-
-    // We'll send message type 2
-    sbuf.mtype = 2;
-    strcpy(sbuf.record,"09/22/21  Windshield   Repair  H Peck      JJDoe   $400  1.5   $60    $460  "); 
-    buf_length = strlen(sbuf.record) + sizeof(int)+1;//struct size without
-    // Send a message.
-    if((msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT)) < 0) {
-        int errnum = errno;
-        fprintf(stderr,"%d, %ld, %s %d\n", msqid, sbuf.mtype, sbuf.record, (int)buf_length);
-        perror("(msgsnd)");
-        fprintf(stderr, "Error sending msg: %s\n", strerror( errnum ));
-        exit(1);
-    }
-    else
-        fprintf(stderr,"msgsnd-report_record: record\"%s\" Sent (%d bytes)\n", sbuf.record,(int)buf_length);
-
-    // We'll send message type 2
-    sbuf.mtype = 2;
-    strcpy(sbuf.record,"Repair P123-23 $24.95 $175 12345698452");
-    buf_length = strlen(sbuf.record) + sizeof(int)+1;//struct size without
-    // Send a message.
-    if((msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT)) < 0) {
-        int errnum = errno;
-        fprintf(stderr,"%d, %ld, %s, %d\n", msqid, sbuf.mtype, sbuf.record, (int)buf_length);
-        perror("(msgsnd)");
-        fprintf(stderr, "Error sending msg: %s\n", strerror( errnum ));
-        exit(1);
-    }
-    else
-        fprintf(stderr,"msgsnd-report_record: record\"%s\" Sent (%d bytes)\n", sbuf.record,(int)buf_length);
-
-    // We'll send message type 2
-    sbuf.mtype = 2;
-    sbuf.record[0]=0;
-    buf_length = strlen(sbuf.record) + sizeof(int)+1;//struct size without
-    // Send a message.
-    if((msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT)) < 0) {
-        int errnum = errno;
-        fprintf(stderr,"%d, %ld, %s, %d\n", msqid, sbuf.mtype, sbuf.record, (int)buf_length);
-        perror("(msgsnd)");
-        fprintf(stderr, "Error sending msg: %s\n", strerror( errnum ));
-        exit(1);
-    }
-    else
-        fprintf(stderr,"msgsnd-report_record: record\"%s\" Sent (%d bytes)\n", sbuf.record,(int)buf_length);
-
+    
+    sem_wait(&s);
+    pthread_cond_signal(&condVar);
+    pthread_join(thread_id, NULL);
 
     exit(0);
 }
